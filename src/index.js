2be09e9f94f00e184c5eac3538d24e18704cc859
yuya1191994 <51263536+yuya1191994@users.noreply.github.com>
2023-06-13 15:13:00 +0900

    p98

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import ReactDOM from "react-dom";
 
 const App = () => {
-    return <h1>こんにちは！</h1>;
+    return (
+        <>
+            <h1>こんにちは！</h1>
+            <p>お元気ですか？</p>
+        </>
+    );
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
