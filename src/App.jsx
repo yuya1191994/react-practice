027945a7f22ae0c06c1c190c781264872341d9bb
yuya1191994 <51263536+yuya1191994@users.noreply.github.com>
2023-06-13 15:48:27 +0900

    p112

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,8 @@ export const App = () => {
         <>
             { console.log('Test') }
             <h1 style={{color: 'red'}}>こんにちは！</h1>
-            <ColoredMessage color='blue' message='お元気ですか？' />
-            <ColoredMessage color='pink' message='元気です！' />
+            <ColoredMessage color='blue'>お元気ですか？</ColoredMessage>
+            <ColoredMessage color='pink'>元気です！</ColoredMessage>
             <button onClick={onClickButton}>Click!</button>
         </>
     );
--- src/components/ColoredMessage.jsx
+++ src/components/ColoredMessage.jsx
@@ -5,5 +5,5 @@ export const ColoredMessage = (props) => {
         fontSize: '20px'
     };
 
-    return <p style={contentStyle}>{props.message}</p>
+    return <p style={contentStyle}>{props.children}</p>
 };
