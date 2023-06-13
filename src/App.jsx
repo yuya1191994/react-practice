a42f7044e4144b125720a00fe9ffe552748135ef
yuya1191994 <51263536+yuya1191994@users.noreply.github.com>
2023-06-13 16:33:53 +0900

    p121

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import ReactDOM from "react-dom";
 import { ColoredMessage } from "./components/ColoredMessage";
 import { useState } from "react";
+import { useEffect } from "react";
 
 export const App = () => {
+    console.log('レンダリング')
+    
     const [num, setNum] = useState(0);
+
+    useEffect(() => {
+        alert();
+    }, [num]);
 
     const onClickButton = () => {
         setNum(num + 1);
@@ -16,7 +23,6 @@ export const App = () => {
             <ColoredMessage color='blue'>お元気ですか？</ColoredMessage>
             <ColoredMessage color='pink'>元気です！</ColoredMessage>
             <button onClick={onClickButton}>Click!</button>
-            <button onClick={onClickButton2}>Click!</button>
             <p>{num}</p>
         </>
     );
